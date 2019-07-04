63055d91cadf2a91e7ed20f59465907f77cec3dc
Tan Yee Jian <tanyeejian@gmail.com>
2019-07-04 12:00:02 +0800

    automated backup

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ function update_packages {
     sudo apt update && sudo apt upgrade -y
 }
 
-install_programs() {
+function install_programs {
     cd scripts # TODO: add a scripts folder
     chmod +x *
     # you can commented out unwanted lines.
@@ -23,11 +23,11 @@ install_programs() {
     ./install_alacritty #alacritty
     ./install_i3wm #i3, fontawesome, i3lock, i3blocks
     ./install_cli_alternatives #alternatives: rg, fzf, fasd, fd
-    ./install_nvim #
+    ./install_nvim #vim, nvim, plugins
     ./install_vscodium #vscodium
 }
 
-main() {
+function main {
     update_packages
     install_programs
     stow_packages
---
+++ vim/.vim/myvimrc.d/plugin/ale.vim
@@ -0,0 +1,3 @@
+" Ctrl-j and Ctrl-k to navigate between errors
+nmap <silent> <C-k> <Plug>(ale_previous_wrap)
+nmap <silent> <C-j> <Plug>(ale_next_wrap)
