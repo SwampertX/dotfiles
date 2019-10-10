bb9f77dc15fd246391ae6391464a563eaee52557
Tan Yee Jian <tanyeejian@gmail.com>
2019-10-10 14:00:02 +0800

    automated backup

---
+++ cron/.cron/backup_dotfiles.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /home/titanx/dotfiles && git add . && git commit -m "automated backup" && git push origin master
---
+++ cron/.cron/luminus_download.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /home/titanx/projects/fluminus_cli && git pull && mix deps.get && mix fluminus --download-to=/home/titanx/nus/ivle/y2s1
