187000c491366b7e55e521fd142dcf9cff235bd2
Tan Yee Jian <tanyeejian@gmail.com>
2019-12-08 18:00:01 +0800

    Automated backup using backup.sh

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -6,7 +6,10 @@
 (after! org
   (map! :map org-mode-map
         :n "M-j" #'org-metadown
-        :n "M-j" #'org-metaup)
+        :n "M-j" #'org-metaup
+        :n "S-k" #'org-shiftup
+        :n "S-j" #'org-shiftdown
+        )
   (setq org-todo-keywords '((sequence "TODO(t)" "INPROGRESS(i)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)"))
         org-agenda-files (list "~/projects/org/")
         org-directory "~/projects/org/")
