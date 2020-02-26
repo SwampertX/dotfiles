a38c358c8863b122826ac6db10c9ab2ae6133aff
Tan Yee Jian <tanyeejian@gmail.com>
2020-02-26 17:00:01 +0800

    Automated backup using backup.sh

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -57,7 +57,7 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
+       (dired +ranger)     ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        vc                ; version-control and Emacs, sitting in a tree
 
