57f5989d609cbdd80861619b4dc5aeae8c3bfc17
Tan Yee Jian <tanyeejian@gmail.com>
2020-04-20 16:00:01 +0800

    Automated backup using backup.sh

--- emacs/.doom.d/packages.el
+++ emacs/.doom.d/packages.el
@@ -4,6 +4,4 @@
 ;;; Examples:
 ;; (package! some-package)
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
-(package! gruvbox-theme)
-(package! org-gcal)
 (package! evil-snipe :disable t)
