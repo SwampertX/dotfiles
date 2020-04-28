fd9f46ecfc877563d76fb16a511d98db1eb27089
Tan Yee Jian <tanyeejian@gmail.com>
2020-04-28 12:00:01 +0800

    Automated backup using backup.sh

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -65,3 +65,6 @@
   (setq org-gcal-client-id "473532847251-hpakf41dv1dvqa9bjg9rd58hl9g4to89.apps.googleusercontent.com"
       org-gcal-client-secret "bNEwNr0PSUze39zqUQPmxtqs"
       org-gcal-file-alist '(("tanyeejian@gmail.com" .  "~/Dropbox/org/cal.org"))))
+
+(after! rustic
+  (setq rustic-lsp-server 'rust-analyzer))
