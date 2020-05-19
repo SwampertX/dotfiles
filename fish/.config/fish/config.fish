6ff6b299a8c8622938d3e5ae2b7d8831f91a36cb
Tan Yee Jian <tanyeejian@gmail.com>
2020-05-19 16:00:02 +0800

    Automated backup using backup.sh

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -36,3 +36,9 @@ set GTYPIST_PATH ~/usr/share/gtypist
 
 # thefuck --alias | source
 # starship init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/titanx/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
