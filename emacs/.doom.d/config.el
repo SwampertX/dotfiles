a3a11bfd642552b7484de769421307baef82aa03
Tan Yee Jian <tanyeejian@gmail.com>
2020-06-18 12:00:01 +0800

    Automated backup using backup.sh

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -96,3 +96,16 @@
 ;;               latex-mode
 ;;               )
 ;;         ))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e2acbf379aa541e07373395b977a99c878c30f20c3761aac23e9223345526bcc" "632694fd8a835e85bcc8b7bb5c1df1a0164689bc6009864faed38a9142b97057" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
