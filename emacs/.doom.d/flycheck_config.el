272f02a24d0d4b5fb11e01d89c4aa07d2698368a
Tan Yee Jian <tanyeejian@gmail.com>
2020-07-03 20:00:02 +0800

    Automated backup using backup.sh

--- emacs/.doom.d/flycheck_config.el
+++ emacs/.doom.d/flycheck_config.el
@@ -96,6 +96,7 @@
               latex-mode
               )
         ))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
