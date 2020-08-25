f2cc9463c3aa07aa4c4ec3ef436141e664f1b912
Tan Yee Jian <tanyeejian@gmail.com>
2020-08-25 12:00:02 +0800

    Automated backup using backup.sh

--- cron/.cron/luminus_download.sh
+++ cron/.cron/luminus_download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cd /home/titanx/projects/fluminus_cli && git pull && mix deps.get && mix fluminus --download-to=/home/titanx/nus/ivle/y3s1
-/home/titanx/bin/fluminurs --download-to=/home/titanx/nus/ivle/y3s1
+/home/titanx/bin/fluminurs --download-to=/home/titanx/nus/ivle/y3s1 --updated=rename
