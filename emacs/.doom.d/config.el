17116e546937a0774b68094eb355c087d73006d8
Tan Yee Jian <tanyeejian@gmail.com>
2020-09-23 12:00:01 +0800

    Automated backup using backup.sh

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -59,7 +59,8 @@
                        ("\\.pdf\\'" . "zathura %s"))
         org-journal-dir "~/Dropbox/org/journal"
         org-roam-directory "~/Dropbox/org/roam"
-        ;; org-journal-file-type 'weekly
+        org-capture-templates `(("i" "inbox" entry (file ,(concat jethro/org-agenda-directory "inbox.org"))
+                                 "* TODO %?")
         )
 )
 
@@ -98,18 +99,5 @@
 ;;               )
 ;;         ))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("e2acbf379aa541e07373395b977a99c878c30f20c3761aac23e9223345526bcc" "632694fd8a835e85bcc8b7bb5c1df1a0164689bc6009864faed38a9142b97057" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (add-hook 'c++-mode-hook #'clang-format+-mode)
