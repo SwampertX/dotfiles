5e7b2d88e00edfcdf0ccd93270fcde0d74183499
Tan Yee Jian <tanyeejian@gmail.com>
2021-02-15 20:00:01 +0800

    Automated backup using backup.sh

--- emacs/.doom.d/custom.el
+++ emacs/.doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("e2acbf379aa541e07373395b977a99c878c30f20c3761aac23e9223345526bcc" "632694fd8a835e85bcc8b7bb5c1df1a0164689bc6009864faed38a9142b97057" default))
+   '("4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" "e2acbf379aa541e07373395b977a99c878c30f20c3761aac23e9223345526bcc" "632694fd8a835e85bcc8b7bb5c1df1a0164689bc6009864faed38a9142b97057" default))
  '(package-selected-packages '(clang-format+)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
