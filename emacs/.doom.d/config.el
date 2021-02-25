d0fb616174337f3ab543ad0953178a8a7edc2a53
Tan Yee Jian <tanyeejian@gmail.com>
2021-02-25 16:00:01 +0800

    Automated backup using backup.sh

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -13,7 +13,7 @@
        (org-agenda-clock-in))
 
 
-(defun yeejian/org-start-work () "Stop working on an item in Agenda."
+(defun yeejian/org-stop-work () "Stop working on an item in Agenda."
        (org-agenda-todo "WAITING")
        (org-agenda-clock-out))
 
@@ -25,65 +25,66 @@
         :n "S-j" #'org-shiftdown
         )
   (map! :map org-agenda-mode-map
-        :n "I" (org-agenda-clock-in)))
-(setq org-todo-keywords '((sequence "TODO(t)" "INPROGRESS(i)" "WAITING(w)" "|" "DONE(d!)" "CANCELLED(c@)"))
-      org-agenda-files (list "~/Dropbox/org/")
-      org-directory "~/Dropbox/org/"
-      org-log-done-with-time t
-      org-list-allow-alphabetical t
-      org-format-latex-header (concat "\\documentclass{article}"
-                                      "\\usepackage[usenames]{color}"
-                                      "[PACKAGES]"
-                                      "[DEFAULT-PACKAGES]"
-                                      "\\pagestyle{empty}             % do not remove"
-                                      "% The settings below are copied from fullpage.sty"
-                                      "\\setlength{\\textwidth}{\\paperwidth}"
-                                      "\\addtolength{\\textwidth}{-3cm}"
-                                      "\\setlength{\\oddsidemargin}{1.5cm}"
-                                      "\\addtolength{\\oddsidemargin}{-2.54cm}"
-                                      "\\setlength{\\evensidemargin}{\\oddsidemargin}"
-                                      "\\setlength{\\textheight}{\\paperheight}"
-                                      "\\addtolength{\\textheight}{-\\headheight}"
-                                      "\\addtolength{\\textheight}{-\\headsep}"
-                                      "\\addtolength{\\textheight}{-\\footskip}"
-                                      "\\addtolength{\\textheight}{-3cm}"
-                                      "\\setlength{\\topmargin}{1.5cm}"
-                                      "\\addtolength{\\topmargin}{-2.54cm}"
-                                      "\\newtheorem{theorem}{Theorem}[section]"
-                                      "\\newtheorem{corollary}{Corollary}[theorem]"
-                                      "\\newtheorem{lemma}{Lemma}[section]"
-                                      "\\newtheorem{note}{Note}[theorem]"
-                                      "\\newtheorem{definition}{Definition}[section]"
-                                      "\\newtheorem{ex}{Example}[section]"
-                                      "\\newtheorem{observation}{Observation}[section]"
-                                      "\\DeclareMathOperator{\\diam}{diam}"
-                                      "\\linespread{1.1}")
-      org-latex-packages-alist '(("" "amsthm" nil) ("" minted nil))
-      bibtex-dialect 'biblatex
-      org-latex-pdf-process '("latexmk -shell-escape -bibtex -pdf %f")
-      org-latex-listings 'minted
-      org-file-apps '((auto-mode . emacs)
-                      (directory . emacs)
-                      ("\\.mm\\'" . default)
-                      ("\\.x?html?\\'" . default)
-                      ("\\.pdf\\'" . "zathura %s"))
-      org-journal-dir "~/Dropbox/org/journal"
-      org-roam-directory "~/Dropbox/org/roam"
-      org-capture-templates
-      '(("t" "Todo" entry (file "inbox.org")
-         "* TODO %?\n  %i\n  %a")
-        ("c" "org-protocol-capture" entry (file "inbox.org")
-         "* TODO [[%:link][%:description]]\n\n %i" :immediate-finish t))
-      org-agenda-custom-commands
-      '(
-        ("x" "Doing and Agenda" ((todo "INPROGRESS|WAITING")(agenda "")))
+        :m "I" #'yeejian/org-start-work
+        :m "O" #'yeejian/org-stop-work)
+  (setq org-todo-keywords '((sequence "TODO(t)" "INPROGRESS(i)" "WAITING(w)" "|" "DONE(d!)" "CANCELLED(c@)"))
+        org-agenda-files (list "~/Dropbox/org/")
+        org-directory "~/Dropbox/org/"
+        org-log-done-with-time t
+        org-list-allow-alphabetical t
+        org-format-latex-header (concat "\\documentclass{article}"
+                                        "\\usepackage[usenames]{color}"
+                                        "[PACKAGES]"
+                                        "[DEFAULT-PACKAGES]"
+                                        "\\pagestyle{empty}             % do not remove"
+                                        "% The settings below are copied from fullpage.sty"
+                                        "\\setlength{\\textwidth}{\\paperwidth}"
+                                        "\\addtolength{\\textwidth}{-3cm}"
+                                        "\\setlength{\\oddsidemargin}{1.5cm}"
+                                        "\\addtolength{\\oddsidemargin}{-2.54cm}"
+                                        "\\setlength{\\evensidemargin}{\\oddsidemargin}"
+                                        "\\setlength{\\textheight}{\\paperheight}"
+                                        "\\addtolength{\\textheight}{-\\headheight}"
+                                        "\\addtolength{\\textheight}{-\\headsep}"
+                                        "\\addtolength{\\textheight}{-\\footskip}"
+                                        "\\addtolength{\\textheight}{-3cm}"
+                                        "\\setlength{\\topmargin}{1.5cm}"
+                                        "\\addtolength{\\topmargin}{-2.54cm}"
+                                        "\\newtheorem{theorem}{Theorem}[section]"
+                                        "\\newtheorem{corollary}{Corollary}[theorem]"
+                                        "\\newtheorem{lemma}{Lemma}[section]"
+                                        "\\newtheorem{note}{Note}[theorem]"
+                                        "\\newtheorem{definition}{Definition}[section]"
+                                        "\\newtheorem{ex}{Example}[section]"
+                                        "\\newtheorem{observation}{Observation}[section]"
+                                        "\\DeclareMathOperator{\\diam}{diam}"
+                                        "\\linespread{1.1}")
+        org-latex-packages-alist '(("" "amsthm" nil) ("" minted nil))
+        bibtex-dialect 'biblatex
+        org-latex-pdf-process '("latexmk -shell-escape -bibtex -pdf %f")
+        org-latex-listings 'minted
+        org-file-apps '((auto-mode . emacs)
+                        (directory . emacs)
+                        ("\\.mm\\'" . default)
+                        ("\\.x?html?\\'" . default)
+                        ("\\.pdf\\'" . "zathura %s"))
+        org-journal-dir "~/Dropbox/org/journal"
+        org-roam-directory "~/Dropbox/org/roam"
+        org-capture-templates
+        '(("t" "Todo" entry (file "inbox.org")
+           "* TODO %?\n  %i\n  %a")
+          ("c" "org-protocol-capture" entry (file "inbox.org")
+           "* TODO [[%:link][%:description]]\n\n %i" :immediate-finish t))
+        org-agenda-custom-commands
+        '(
+          ("x" "Doing and Agenda" ((todo "INPROGRESS|WAITING")(agenda "")))
+          )
+        org-global-properties '(("Effort_ALL" . "0 0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00"))
+        org-columns-default-format "%58ITEM %TODO %3PRIORITY %TAGS %5Effort(Est.){:} %CLOCKSUM(Spent)"
+        ;; org-columns-default-format-for-agenda "%25ITEM %TODO %3PRIORITY %TAGS %17Effort(Estimated Effort){:} %CLOCKSUM"
+        ;; org-columns-default-format-for-agenda nil
         )
-      org-global-properties '(("Effort_ALL" . "0 0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00"))
-      org-columns-default-format "%60ITEM %TODO %3PRIORITY %TAGS %Effort(Est.){:} %CLOCKSUM(Spent)"
-      ;; org-columns-default-format-for-agenda "%25ITEM %TODO %3PRIORITY %TAGS %17Effort(Estimated Effort){:} %CLOCKSUM"
-      ;; org-columns-default-format-for-agenda nil
-      )
-)
+  )
 
 (after! pdf-tools
   (map! :map pdf-view-mode-map
