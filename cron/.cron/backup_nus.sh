9ff5593055e4e245d272cf43e8da3392fb27402f
Tan Yee Jian <tanyeejian@gmail.com>
2021-03-01 12:00:01 +0800

    Automated backup using backup.sh

--- cron/.cron/backup_nus.sh
+++ cron/.cron/backup_nus.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/titanx/projects/org && git add . && git commit -m "automated update" && git push origin master
+cd /home/titanx/nus && git commit -am "automated update" && git push origin master
