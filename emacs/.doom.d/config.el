2caf453cc3eeebf4412beaebdf3fe3388888ac94
Tan Yee Jian <yeejian.tan@u.nus.edu>
2021-10-07 13:26:02 +0800

    Add emacs snippets, change doom packages

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -81,7 +81,8 @@
           ("x" "Doing and Agenda" ((todo "INPROGRESS|WAITING")(agenda "")))
           )
         org-global-properties '(("Effort_ALL" . "0 0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00"))
-        org-columns-default-format "%58ITEM %TODO %3PRIORITY %TAGS %5Effort(Est.){:} %CLOCKSUM(Spent)"
+        org-columns-default-format "%58ITEM %TODO %3PRIORITY %5Effort(Est.){:} %CLOCKSUM(Spent){:}"
+        org-pomodoro-audio-player "paplay --volume=52000"
         ;; org-columns-default-format-for-agenda "%25ITEM %TODO %3PRIORITY %TAGS %17Effort(Estimated Effort){:} %CLOCKSUM"
         ;; org-columns-default-format-for-agenda nil
         org-download-method 'directory
@@ -168,4 +169,5 @@
 (after! mixed-pitch-mode
   (setq )
   )
+
 (message "Done reloading config.")
